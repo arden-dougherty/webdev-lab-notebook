63b10f03534cdd785da80c415a6aba5b8c099540
Arden Dougherty <asd6@pdx.edu>
2025-05-20 15:14:25 -0700

    bootstrap

--- 07-javascript/04-dom.js
+++ 07-javascript/04-dom.js
@@ -2,16 +2,26 @@
 // when the user clicks the 'copy' button, copy the user input to the output area
 
 // fetch JavaScript objects representing specific elements in the DOM
+let output = document.querySelector(".output");
+let input1 = document.getElementById("userInput1");
 
 // add an event listener on the target element
-
+document.getElementById("copy").onclick = function (event) {
+  output.textContent = input1.value;
+};
 // callback function to handle event
 
 // Exercise #2:
 // when the user enters input text, copy the user input to the output area
 
 // fetch JavaScript objects representing specific elements in the DOM
+let input2 = document.getElementById("userInput2");
+let section2 = document.getElementById("inputEventExample");
+let output2 = document.createElement("div");
+section2.append(output2);
 
 // add an event listener on the target element
-
+input2.oninput = function (event) {
+  output2.textContent = input2.value;
+};
 // callback function to handle event
--- reflection.md
+++ reflection.md
@@ -10,4 +10,12 @@ We had covered most of the tools needed to complete this exercise in class and i
 
 ### CSS
 
-Enter your reflections on this section here.
+I think these exercises were very helpful for learning the fundamentals of CSS. I used the code samples a lot to help with the exercises. The most difficult for me was the position exercise, and getting the absolute and fixed positions to work correctly.
+
+### Flexbox
+
+### Grid
+
+### Bootstrap
+
+To get started with the first bootstrap exercise I had to watch the video, but after that I found the exercises fairly simple to complete. I found the bootstrap grid fairly straightforward after having experience with the normal CSS grid and flexbox.
